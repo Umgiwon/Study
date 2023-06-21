daaf8615ceeacfb219923aa01a4cb02075524860
Umgiwon <ugwzzang@daum.net>
2023-06-21 11:57:38 +0900

    230621 #1

--- REACT/step3/react-for-test/src/App.js
+++ REACT/step3/react-for-test/src/App.js
@@ -1,7 +1,19 @@
+import { useState, useEffect } from "react";
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("run all the time");
+
+  /** useEffect는 최초 Render 시 한번만 작동할 수 있도록 처리 */
+  useEffect(() => {
+    console.log("run only once");
+  }, []);
+
   return (
     <div>
-      <h1>Welcome to the react</h1>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>clicke</button>
     </div>
   );
 }
