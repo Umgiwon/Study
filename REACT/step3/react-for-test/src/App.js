ffeb3b5db801eb8804e646003aec165edef4f7f3
Umgiwon <ugwzzang@daum.net>
2023-06-21 15:09:22 +0900

    230621 #2

--- REACT/step3/react-for-test/src/App.js
+++ REACT/step3/react-for-test/src/App.js
@@ -1,17 +1,33 @@
 import { useState, useEffect } from "react";
 
 function App() {
+  /** state */
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
+  /** event */
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
   console.log("run all the time");
 
   /** useEffect는 최초 Render 시 한번만 작동할 수 있도록 처리 */
   useEffect(() => {
     console.log("run only once");
   }, []);
+  useEffect(() => {
+      console.log("run when 'keyword' changes");
+  }, [keyword]);
+  useEffect(() => {
+      console.log("run when 'counter' changes");
+  }, [counter]);
+  useEffect(() => {
+      console.log("run when 'counter || keyword' changes");
+  }, [keyword, counter]);
 
   return (
     <div>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here" />
       <h1>{counter}</h1>
       <button onClick={onClick}>clicke</button>
     </div>
